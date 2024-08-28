3d1064e137547b6be1104e5ab4a7658037005ee4
tesddev <tesleem.amuda@gmail.com>
2024-08-28 18:12:20 +0100

    add all files

---
+++ Tagging/Model/Tag.swift
@@ -0,0 +1,8 @@
+//
+//  Tag.swift
+//  Tagging
+//
+//  Created by GIGL-MAC on 28/08/2024.
+//
+
+import Foundation
---
+++ Tagging/View/TagView.swift
@@ -0,0 +1,18 @@
+//
+//  TagView.swift
+//  Tagging
+//
+//  Created by GIGL-MAC on 28/08/2024.
+//
+
+import SwiftUI
+
+struct TagView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TagView()
+}
