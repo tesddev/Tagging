857077507799ad954868be0bdbbc1567b9f2b42e
tesddev <tesleem.amuda@gmail.com>
2024-08-28 13:22:22 +0100

    Initial Commit

---
+++ Tagging/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tagging
+//
+//  Created by GIGL-MAC on 28/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tagging/TaggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaggingApp.swift
+//  Tagging
+//
+//  Created by GIGL-MAC on 28/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TaggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
